0f4204aafd1fac5b65e06b28ee0fa6955228f08c
Kate Brown <kbrown8158@gmail.com>
2017-04-01 16:58:05 -0400

    added user model tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "person#{n}@example.com" }
+    first_name 'Kate'
+    last_name 'Brown'
+    sequence(:username) { |n| "username #{n}"}
+    password 'password'
+    password_confirmation 'password'
   end
 end
